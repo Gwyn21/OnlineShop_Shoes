c438d5508ffbb066baa0838e33f2dc089ba4148c
AnNTHE173189 <annthe173189@fpt.edu.vn>
2025-03-05 15:27:47 +0700

    commit

--- src/main/java/org/example/swp391/entity/Account.java
+++ src/main/java/org/example/swp391/entity/Account.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 @Table(name = "account")
-public class Account {
+public class    Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
